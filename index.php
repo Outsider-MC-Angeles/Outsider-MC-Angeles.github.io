e94bd0463ab65b78d87543da25cc3c6c816dd197
Outsider-MC-Angeles <161560044+Outsider-MC-Angeles@users.noreply.github.com>
2024-02-29 14:20:40 +0800

    240229_1420

---
+++ index.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Outsider MC - Angeles</title>
+<meta name="generator" content="WYSIWYG Web Builder 19 - https://www.wysiwygwebbuilder.com">
+<link href="OMC_head_16x16.ico" rel="shortcut icon" type="image/x-icon">
+<link href="240228_OMC.css" rel="stylesheet">
+<link href="index.css" rel="stylesheet">
+<script src="bootstrap.min.js"></script>
+</head>
+<body data-bs-spy="scroll">
+<div id="container">
+<div id="wb_CssMenu1" style="position:absolute;left:127px;top:13px;width:671px;height:40px;z-index:0;">
+<ul id="CssMenu1" role="menubar" class="nav">
+<li role="menuitem" class="nav-item firstmain"><a class="nav-link" href="./index.php" target="_self">Home</a>
+</li>
+<li role="menuitem" class="nav-item"><a class="nav-link" href="./history.html" target="_self">History</a>
+</li>
+<li role="menuitem" class="nav-item"><a class="nav-link" href="./events.html" target="_self">Events</a>
+</li>
+<li role="menuitem" class="nav-item"><a class="nav-link" href="./BGBNF.html" target="_self">BGBNF</a>
+</li>
+<li role="menuitem" class="nav-item"><a class="nav-link" href="./111-shop.html" target="_self">111-Shop</a>
+</li>
+<li role="menuitem" class="nav-item"><a class="nav-link" href="./gallery.html" target="_self">Gallery</a>
+</li>
+<li role="menuitem" class="nav-item"><a class="nav-link" href="./links.html" target="_self">Links</a>
+</li>
+<li role="menuitem" class="nav-item"><a class="nav-link" href="./contact.html" target="_self">Contact</a>
+</li>
+<li role="menuitem" class="nav-item"><a class="nav-link" href="./disclaimer.html" target="_self">Disclaimer</a>
+</li>
+</ul>
+</div>
+<div id="wb_Image1" style="position:absolute;left:69px;top:98px;width:144px;height:171px;z-index:1;">
+<img src="images/OMC_patch_001.png" id="Image1" alt="" width="144" height="172"></div>
+<div id="wb_Text1" style="position:absolute;left:255px;top:98px;width:521px;height:384px;z-index:2;">
+<p style="font-size:15px;line-height:16.5px;"><span style="font-size:21px;line-height:24.5px;font-weight:bold;">Outsider MC Angeles City Chapter ......</span></p>
+<p style="font-size:15px;line-height:16.5px;">&nbsp;</p>
+<p style="font-size:15px;line-height:16.5px;">&nbsp;</p>
+<p style="font-size:15px;line-height:16.5px;">........ opened on the 1st July 2016 by some of the officers and members of the Laguna Chapter who resided in Angeles City and Marinduque. </p>
+<p style="font-size:15px;line-height:16.5px;">&nbsp;</p>
+<p style="font-size:15px;line-height:16.5px;">It was felt at that time to be the appropriate time to expand the Outsider MC brotherhood in the Philippines and open an OMC Angeles City Chapter.</p>
+<p style="font-size:15px;line-height:16.5px;">&nbsp;</p>
+<p style="font-size:15px;line-height:16.5px;">Although this is a newly formed Chapter its officers and members are long standing residents of the Philippines of over 20 years or more and well known in the motorcycle community of the Philippines. </p>
+<p style="font-size:15px;line-height:16.5px;">&nbsp;</p>
+<p style="font-size:15px;line-height:16.5px;">The new Chapter is formed with officers and members with various nationalities, Austrian, Scottish, Australian, and German and hope to attract expat foreigners and local Filipino riders who meet the criteria and enjoy the brotherhood and comradery associated with Motorcycle riding in the Philippines. </p>
+<p style="font-size:15px;line-height:16.5px;">&nbsp;</p>
+<p style="font-size:15px;line-height:16.5px;">We ride and enjoy the tropical climate and partake in community activities like Charity Rides for the under privileged and attend various bike rally’s and events throughout the country. All bikers are welcome to join us in any of our rides.</p>
+<p style="font-size:15px;line-height:16.5px;">&nbsp;</p>
+<p style="font-size:15px;line-height:16.5px;"> ONE WORLD ONE VISION - 111</p></div>
+<!-- FreeHitCounter.org -->
+<div id="Html1" style="position:absolute;left:410px;top:914px;width:162px;height:81px;z-index:3">
+<script src="https://freehitcounters.org/count/dydj"></script><br>
+ <a href='http://hauckautoren.de/'>http://hauckautoren.de</a> <script type='text/javascript' src='https://whomania.com/ctr?id=8fd0240c5c50d419559a17a6061952bc326de4be'></script></div>
+<div id="wb_Extension1" style="position:absolute;left:351px;top:792px;width:281px;height:18px;z-index:4;">
+<span style="color:#FFFFFF;font-family:Verdana;font-size:11px;">
+<?php
+   $file = fopen('counter.txt', 'r');
+   $data = fread($file, filesize('counter.txt'));
+   fclose($file);
+   if ($data !== false)
+   {
+      $hits = intval($data);
+      $hits++;
+      $file = fopen('counter.txt', 'w');
+      flock($file, LOCK_EX);
+      fwrite($file, $hits);
+      flock($file, LOCK_UN);
+      fclose($file);
+   }
+   echo "This page has been visited $hits times!";
+?>
+</span>
+</div>
+</div>
+</body>
+</html>
